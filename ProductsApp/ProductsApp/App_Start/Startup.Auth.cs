08869fc540525f235c8b1240248fdf1978fc2d7a
Jasneet Bhatti <jabhatti@microsoft.com>
2016-05-22 22:20:29 +0530

    Added Facebook auth

--- ProductsApp/ProductsApp/App_Start/Startup.Auth.cs
+++ ProductsApp/ProductsApp/App_Start/Startup.Auth.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Facebook;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using ProductsApp.Models;
@@ -54,9 +55,11 @@ namespace ProductsApp
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(new FacebookAuthenticationOptions
+            {
+                AppId = "",
+                AppSecret = ""
+            });
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
