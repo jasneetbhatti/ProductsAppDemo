49f6e61f6135a3c95bd7e443a0d85154a6edcca9
Jasneet Bhatti <jabhatti@microsoft.com>
2016-05-22 20:27:18 +0530

    Added Products model

---
+++ ProductsApp/ProductsApp/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductsApp.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+    }
+}
